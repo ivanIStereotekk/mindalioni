ec9528c5f12ae63c9098839ff96ee987029a18d1
Ivan Goncharov <ivan.stereotekk@gmail.com>
2025-06-02 17:44:15 +0400

    content: some pages addded

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -5,26 +5,36 @@ export interface Project {
     url: string;
   }
   
+
+  
+
   export const projects: Project[] = [
     {
-      title: "Mindalioni Technopolis",
-      year: 2023,
+      title: "Georgia - Better place for build your dream",
+      year: 2024,
       description:
-        "Mindalioni Technopolis",
-      url: "https://youtube.com",
+        "Georgia is a country in Eastern Europe and West Asia. It is part of the Caucasus region, bounded by the Black Sea to the west, Russia to the north and northeast, Turkey to the southwest, Armenia to the south, and Azerbaijan to the southeast. ",
+      url: "https://en.wikipedia.org/wiki/Georgia_(country)",
     },
     {
-      title: "Mindalioni Technopolis",
+      title: "Preamble article",
+      year: 2024,
+      description:
+        "In April 2025, Georgia stands at a crossroads. On one hand, the country's economy demonstrates sustainable growth (GDP of approximately $20 billion, with an annual growth of 6-8% according to IMF data), its geopolitical position offers opportunities, and its cultural heritage remains a rich resource. However, dependence on imported technologies, poverty (16% of the population according to 2023 data), and the continuous outflow of young people impede the country's development.",
+      url: "http://geopolis.tech/contact",
+    },
+    {
+      title: "Mindalioni Technopolis - Structure of the Technopolis: Universities and Research Centers",
       year: 2022,
       description:
-        "Mindalioni Technopolis",
-      url: "https://www.google.com",
+        "The educational structure of the Mindalioni Technopolis is designed to create a comprehensive learning and research environment that fosters innovation, preserves cultural heritage, and prepares students for future challenges. Through collaboration between different institutions and research centers, the technopolis aims to become a hub of knowledge creation and dissemination. ",
+      url: "http://geopolis.tech/conversation",
     },
     {
-      title: "Mindalioni Technopolis",
+      title: "Geopolis blog page short review",
       year: 2021,
       description:
-        "Mindalioni Technopolis",
-      url: "http://geopolis.tech",
+        "The Mindalioni Technopolis represents a bold vision for Georgia's future - a unique blend of technological innovation and cultural preservation, where tradition meets progress, and where the country's intellectual potential can flourish.",
+      url: "http://geopolis.tech/blog",
     },
   ];
